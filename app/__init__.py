4595c025b1583791b6b58e1548e8e88f8c5e9d08
Quewayne Seaton <quewayneseaton@yahoo.com>
2018-03-22 00:53:06 +0000

    part 2  complete

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "pineapple"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:Pineapple123@localhost/userdb"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:Pineapple123@localhost/userdb"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://mlgrsqdyybzxnu:f361e4400806c6c92fb832dc1e196cb4e213f906ffeba7ffbc0a6c8bb64aeb81@ec2-23-21-217-27.compute-1.amazonaws.com:5432/derlilg3evp6ko"
 
 
 db = SQLAlchemy(app)
